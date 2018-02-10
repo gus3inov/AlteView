1e00d84418bd9507d3613c58147b26dcfa2d34ea
gus3inov <gusejnov1999@list.ru>
2018-02-10 19:38:50 +0300

    actions contstance

---
+++ src/constance.ts
@@ -0,0 +1,8 @@
+export const INCREMENT = 'app/INCREMENT'
+export type INCREMENT = typeof INCREMENT
+
+export const DECREMENT = 'app/DECREMENT'
+export type DECREMENT = typeof DECREMENT
+
+export const RESET = 'app/RESET'
+export type RESET = typeof RESET
