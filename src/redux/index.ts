720bdc85ec2d84a7566dfd94a9a1850bdf24dd98
gus3inov <gusejnov1999@list.ru>
2018-02-14 20:28:55 +0300

    change import path

--- src/redux/index.ts
+++ src/redux/index.ts
@@ -1,6 +1,6 @@
 
 import { combineReducers } from 'redux'
-import counterReducer from './counter'
+import counterReducer from '../components/Counter/counter'
 
 export default combineReducers({
     count: counterReducer
