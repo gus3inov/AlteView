5684507ddb9ce1622e1eab9740dd94bff255d02c
gus3inov <gusejnov1999@list.ru>
2018-02-14 20:28:00 +0300

    create HOC for toggle Elements

---
+++ src/hocs/toggleOpen.tsx
@@ -0,0 +1,47 @@
+import * as React from 'react'
+import {Fragment} from "react";
+
+export interface InjectedProps {
+    isOpen: boolean;
+    toggleOpen(): any   ;
+}
+
+export interface ExternalProps {
+    isOpen: boolean;
+}
+
+export interface State {
+    isOpen: boolean;
+}
+
+export default function toggleOpen<OriginProps>(Component: React.ComponentType<OriginProps & InjectedProps>){
+    type ResultProps = OriginProps & ExternalProps
+
+    return class extends React.Component<ResultProps, State>{
+
+        static displayName =  `toggleOpen(${Component.displayName || 'SUKA'})`
+
+        state: State = {
+            isOpen: this.props.isOpen
+        }
+
+        toggleOpen = () => {
+            const { isOpen } = this.props
+
+            this.setState({
+                isOpen: !this.state.isOpen
+            })
+
+            console.log(isOpen)
+        }
+
+        render(){
+            return (
+                <Fragment>
+                    <Component {...this.props} {...this.state} toggleOpen = { this.toggleOpen } />
+                </Fragment>
+            )
+        }
+
+    }
+}
